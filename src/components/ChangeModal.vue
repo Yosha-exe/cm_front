4fa536e6002eb17d1ca7b35f97d32e130680e5fa
Yosha-exe <alexmarkeloff5@gmail.com>
2020-12-12 12:39:56 +0300

    added components for new opportunities

---
+++ src/components/ChangeModal.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "ChangeModal"
+
+
+}
+
+import vmodal from 'vue-js-modal'
+Vue.use(vmodal)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Employee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="member__block">
+      <img src="https://via.placeholder.com/30.png?text=Img" alt="avatar" class="member-avatar">
+      <div class="member-name">Employee name</div>
+    </div>
+</template>
+
+<script>
+export default {
+name: "Employee"
+}
+</script>
+
+<style scoped>
+
+</style>
